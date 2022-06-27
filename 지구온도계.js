d53a110591befe559d8a3a78af76ce68baad7634
hajin <w2128@e-mirim.hs.kr>
2022-06-27 18:23:28 +0900

    06/27 RTT css 완성

--- 지구온도계.js
+++ 지구온도계.js
@@ -1,5 +1,28 @@
 //버튼을 클릭했을 때 card 보이게 하기
-const show_card = (classId) => {
-    //card를 hidden한 id요소 삭제 
-    document.querySelector(classId).removeAttribute('id');
+const show_card = (btnId, rowId) => {
+    //html에서 js로 요소 가져오기
+    const btn = document.getElementById(btnId);
+    const card = document.getElementById(rowId);
+    if(btn){
+        //card를 hidden한 id요소
+        btn.addEventListener('click',()=> {
+        card.classList.toggle('show_row');
+        });
+    }
+    
 };
+const show_card2 = (btnId, rowId) => {
+    //html에서 js로 요소 가져오기
+    const btn = document.getElementById(btnId);
+    const card = document.getElementById(rowId);
+    if(btn){
+        //card를 hidden한 id요소
+        btn.addEventListener('click',()=> {
+        card.classList.toggle('show_row2');
+        });
+    }
+    
+};
+
+show_card("click_btn1","row"); //id이름
+show_card2("click_btn2","row2");
